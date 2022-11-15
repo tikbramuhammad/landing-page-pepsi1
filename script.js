0d1acdbf46f7b0d5e2b599b073c59a1ae2f61eaa
Tikbra <muhammadtikbra182@gmail.com>
2022-11-15 22:52:56 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function imgSlider(aniything) {
+  document.querySelector('.pepsi').src = aniything;
+}
+
+function changeBgColor(color) {
+  const container = document.querySelector('.container');
+  container.style.background = color;
+}
