dbb750e615d2b30870da134d446253e03da1f03d
Tikbra <muhammadtikbra182@gmail.com>
2022-11-16 13:58:59 +0700

    add responsive

--- script.js
+++ script.js
@@ -6,3 +6,10 @@ function changeBgColor(color) {
   const container = document.querySelector('.container');
   container.style.background = color;
 }
+
+function menuToggle(){
+  const toggleMenu = document.querySelector('.toggle-menu');
+  const navigation = document.querySelector('.navigation');
+  toggleMenu.classList.toggle('active');
+  navigation.classList.toggle('active');
+}
